030e34df7b9767508f250b1f3a5d8b3f2da8e090
Valentin LÉVÊQUE <contact@valentinleveque.me>
2016-09-02 00:29:51 +0200

    Wallpapers

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,7 @@ import GapiAuth from './GapiAuth.vue'
 import Site from './Site.vue'
 import Clock from './Clock.vue'
 import './GapiViewSelector2.js'
+import away from 'away'
 
 Vue.config.devtools = true
 Vue.use(VueResource)
@@ -21,7 +22,53 @@ new Vue({
         sites   : config.defaultViews,
     },
 
+    ready() {
+        let body        = this.$els.body
+        let activeTimer = away(3000)
+
+        body.classList.add('user-active')
+
+        activeTimer.on('idle', () => {
+            body.classList.remove('user-active')
+        })
+        activeTimer.on('active', () => {
+            body.classList.add('user-active')
+        })
+
+        this.setWallpaper()
+
+        setInterval(() => {
+
+            this.setWallpaper()
+        }, 1800000)
+    },
+
     methods: {
+
+        setWallpaper() {
+            let body       = this.$els.body
+            let wallpapers = this.getWallpapersPaths()
+            let random     = wallpapers[Math.floor(Math.random() * wallpapers.length)]
+
+            let image = new Image()
+            image.src = random
+
+            image.onload = (e) => {
+                body.style.backgroundImage = 'url(' + random + ')'
+            }
+        },
+
+        getWallpapersPaths() {
+            let path       = '/assets/images/wallpapers/wallpaper-'
+            let ext        = '.jpg'
+            let wallpapers = []
+
+            for (let i = 1; i < 52; i++) {
+                wallpapers.push(path + i + ext)
+            }
+
+            return wallpapers
+        },
 
         addSite() {
 
